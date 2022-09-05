8ea0ac491bbec92e7e2730ce14318a22918f1b5e
Akshay R Panicker <akshayrpanicker005@gmail.com>
2022-09-05 23:23:28 +0530

    admin wedgetdesign

--- client/src/Components/AdminHome/AdminHome.jsx
+++ client/src/Components/AdminHome/AdminHome.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AdminNavbar from '../AdminNavbar/AdminNavbar'
 import Adminsidebar from '../Adminsidebar/Adminsidebar'
+import AdminWidget from '../AdminWidget/AdminWidget'
 import './AdminHome.scss'
 
 function AdminHome() {
@@ -9,7 +10,12 @@ function AdminHome() {
       <Adminsidebar/>
       <div className="homeContainer">
         <AdminNavbar/>
-        home container
+       <div className="widgets">
+        <AdminWidget type = "user"/>
+        <AdminWidget type = "order"/>
+        <AdminWidget type = "erarning"/>
+        <AdminWidget type = "balance"/>
+       </div>
       </div>
     </div>
   )
